18e71396f01d666aa745f2134e8e7d2e258a6d34
Ubuntu <sshuser@pablodlvm.1yqmhognzd0elfaiesevyeed4f.xx.internal.cloudapp.net>
2018-09-10 18:53:43 +0000

    modified AML SDK notebooks to new version/changes

--- deploy/test_service.py
+++ deploy/test_service.py
@@ -3,15 +3,16 @@ import urllib
 import json 
 import requests
 import pandas as pd
+import sys
 
 # The URL will need to be editted after service create.
-url = 'http://40.76.29.12:5001/score'
+url =  sys.argv[1] # gets the url from the console argument 
 
 ## Sequence length will need to match the training sequence length from the model training
 sequence_length = 10
 
 # We'll read in this data to test the service
-body = pd.read_pickle('test_dataframe.pkl')
+body = '{"data": "[{\\"Close\\":276.56,\\"sma_diff\\":0.1256666667,\\"ema_diff\\":-0.9208042383,\\"rsi\\":42.9571116039},{\\"Close\\":275.5,\\"sma_diff\\":1.2423333333,\\"ema_diff\\":-0.5204372189,\\"rsi\\":33.4023302607},{\\"Close\\":275.97,\\"sma_diff\\":1.486,\\"ema_diff\\":-0.3212150757,\\"rsi\\":40.7113516968},{\\"Close\\":274.24,\\"sma_diff\\":1.9463333333,\\"ema_diff\\":0.0610113416,\\"rsi\\":27.0514200019},{\\"Close\\":274.74,\\"sma_diff\\":1.8546666667,\\"ema_diff\\":0.2211086852,\\"rsi\\":34.9380909623},{\\"Close\\":271.0,\\"sma_diff\\":2.7553333333,\\"ema_diff\\":0.844388337,\\"rsi\\":17.3747515618},{\\"Close\\":271.6,\\"sma_diff\\":2.9033333333,\\"ema_diff\\":1.105809008,\\"rsi\\":24.9413127492},{\\"Close\\":269.35,\\"sma_diff\\":3.832,\\"ema_diff\\":1.5421559899,\\"rsi\\":17.4504277746},{\\"Close\\":270.89,\\"sma_diff\\":3.0846666667,\\"ema_diff\\":1.5263620609,\\"rsi\\":34.3259071505},{\\"Close\\":271.28,\\"sma_diff\\":2.6063333333,\\"ema_diff\\":1.3986775304,\\"rsi\\":38.3175942846}]"}'
 headers = {'Content-Type':'application/json'}
 
 try:
@@ -20,7 +21,6 @@ try:
     else:
         #print('{}'.format(body.shape))
         body = json.dumps({"data": body.to_json(orient='records')})
-        print (body + '\n')
         req = urllib.request.Request(url, str.encode(body), headers) 
         
         with urllib.request.urlopen(req) as response:
